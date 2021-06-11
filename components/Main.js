6636ac0833149dd13c32727802a5cdd942093b77
Phil-boter <phildwd@gmail.com>
2021-06-11 15:12:09 +0200

    initital commit

---
+++ components/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+class Main extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Hello world</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({});
+
+export default Main;
---
+++ context/UserContext.js
@@ -0,0 +1,4 @@
+import React from "react";
+import * as firebase from "firebase";
+
+export default (React.createContext < undefined) | (firebase.User > undefined);
